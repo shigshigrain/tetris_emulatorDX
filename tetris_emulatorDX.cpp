316f08891aa52da4cee1e4ca6aa1f2bf2c1bcb2a
soya k <sy.kwsk@gmail.com>
2021-12-23 18:12:33 +0900

    v3.6.2更新

--- tetris_emulatorDX.cpp
+++ tetris_emulatorDX.cpp
@@ -659,7 +659,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     //*/
 
     Window window;
-    window.Create("tetris_emulator_ver3.6.1", "DXlib");
+    window.Create("tetris_emulator_ver3.6.2", "DXlib");
     SetUserWindow(window.GetHandle());
     SetUserWindowMessageProcessDXLibFlag(false);
     SetGraphMode(1200, 800, 32);
